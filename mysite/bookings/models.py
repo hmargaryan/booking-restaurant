835db0a5db4463ef7d66bc2a9986df9fe2e37844
MrMargaryan <hamlet.java@gmail.com>
2021-01-06 21:59:29 +0300

    created models

---
+++ mysite/bookings/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+# Create your models here.
+
+
+class Reservation(models.Model):
+    coming = models.DateTimeField('reservation time')
+    duration = models.IntegerField(
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(24)])
+    guest = models.ForeignKey('users.Guest', on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(
+        'restaurants.Restaurant', on_delete=models.CASCADE)
+
+    def __str__(self):
+        person = self.guest.user.get_full_name(
+        ) if self.guest.user.get_full_name() else self.guest.user.username
+        place = self.restaurant.name
+        time = self.coming
+        duration = self.duration
+        return person + " in " + place + " at " + str(time) + " for " + str(duration) + " hours"
+
+
+class ReservedTable(models.Model):
+    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
+    table = models.ForeignKey('restaurants.Table', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.reservation) + " table: " + str(self.table)
+
+
+class LastVisit(models.Model):
+    tip = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
+    guest = models.ForeignKey('users.Guest', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.reservation) + " for: " + str(self.guest)
---
+++ mysite/restaurants/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+# Create your models here.
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    address = models.CharField(max_length=200, null=True, blank=True)
+    rows = models.IntegerField(validators=[MinValueValidator(1)])
+    columns = models.IntegerField(validators=[MinValueValidator(1)])
+    tables = models.IntegerField(validators=[MinValueValidator(1)])
+    is_closed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Favourite(models.Model):
+    guest = models.ForeignKey('users.Guest', on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+
+    def __str__(self):
+        guest = self.guest.user.get_full_name(
+        ) if self.guest.user.get_full_name() else self.guest.user.username
+        return self.restaurant.name + " (" + guest + ")"
+
+
+class Review(models.Model):
+    guest = models.ForeignKey('users.Guest', on_delete=models.CASCADE)
+    comment = models.CharField(max_length=200)
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+
+    def __str__(self):
+        guest = self.guest.user.get_full_name(
+        ) if self.guest.user.get_full_name() else self.guest.user.username
+        return guest + " review on " + self.restaurant.name
+
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    price = models.FloatField(default=1)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name + " (" + self.restaurant.name + ")"
+
+
+class Table(models.Model):
+    number = models.IntegerField(validators=[MinValueValidator(1)])
+    row = models.IntegerField(validators=[MinValueValidator(1)])
+    column = models.IntegerField(validators=[MinValueValidator(1)])
+    currently_free = models.BooleanField(default=True)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.restaurant) + " " + str(self.number)
